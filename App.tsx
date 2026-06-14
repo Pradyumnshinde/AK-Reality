32b756ba8cf70d48ff283eb0bbc42bd83064ee94
Pradyumnshinde <pradyumnshinde53@gmail.com>
2026-06-14 13:08:11 +0530

    update import paths for flat repository structure

--- App.tsx
+++ App.tsx
@@ -3,13 +3,13 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Location from './components/Location';
-import Footer from './components/Footer';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Location from './Location';
+import Footer from './Footer';
 
 export default function App() {
   return (
